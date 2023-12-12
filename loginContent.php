ee6a8ff586f27c5c3de538cc47d38d5fa18cab47
Kyoungho MIn <khms4802@gmail.com>
2023-12-12 20:56:20 +0800

    Added Rooms and Mates page

---
+++ loginContent.php
@@ -0,0 +1,35 @@
+
+<div class="container py-4 h-auto">
+  <div class="row d-flex justify-content-center align-items-center h-auto">
+    <div class="col-12 col-md-8 col-lg-6 col-xl-5 pb-1">
+
+        <div class="card-body p-5 text-center">
+          <div class="mb-md-5 mt-md-4 pb-1">
+            <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+            <p class="text-secondary mb-5">Welcome Back!</p>
+
+            <div class="form-outline form-white mb-4">
+              <label class="form-label w-100 text-start" for="typeEmailX">User Name</label>
+              <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+            </div>
+
+            <div class="form-outline form-white mb-4">
+              <label class="form-label w-100 text-start" for="typePasswordX">Password</label>
+              <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+            </div>
+
+            <p class="small mb-5 pb-1"><a class="text-secondary" href="#!">Forgot password?</a></p>
+
+            <button class="btn btn-outline-dark btn-lg px-5" type="submit">Login</button>
+          </div>
+
+          <div>
+            <p class="mb-0">Don't have an account? <a href="#!" class="text-primary fw-bold">Sign Up</a>
+            </p>
+          </div>
+        </div>
+
+    </div>
+  </div>
+</div>
+
